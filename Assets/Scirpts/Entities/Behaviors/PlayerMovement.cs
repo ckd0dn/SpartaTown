c1f8bc723ada658b4aa4ddda5a7b064b32032a45
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-05-09 19:04:29 +0900

    feat: 움직임 구현

---
+++ Assets/Scirpts/Entities/Behaviors/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private MainController controller;
+    private Rigidbody2D rigidbody2D;
+
+    private Vector2 moveDirection = Vector2.zero;   
+
+    private void Awake()
+    {
+        controller = GetComponent<MainController>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        moveDirection = direction; 
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMove();
+    }
+
+    private void ApplyMove()
+    {
+        // 속도 추가해야댐
+        float speed = 5f;
+        rigidbody2D.velocity = moveDirection * speed;
+    }
+
+
+
+
+}
---
+++ Assets/Scirpts/Entities/Controllers/PlayerInputController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MainController
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>(); 
+        CallMoveEvent(moveInput);   
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos - (Vector2)transform.position).normalized;
+        CallLookEvent(newAim);
+    }
+
+
+    public void OnClick(InputValue value)
+    {
+
+    }
+
+}
