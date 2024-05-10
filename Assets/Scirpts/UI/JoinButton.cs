ff8e98dc743c2cb9d3bc935110d14e8c05a14840
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2024-05-11 01:08:58 +0900

    feat: 플레이어 이름 입력

--- Assets/Scirpts/UI/JoinButton.cs
+++ Assets/Scirpts/UI/JoinButton.cs
@@ -12,13 +12,16 @@ public class JoinButton : MonoBehaviour
     private void Update()
     {
         playerName = playerNameInput.text;
-        Debug.Log(playerNameInput.text);
     }
 
     public void JoinGame()
     {
-        SceneManager.LoadScene("MainScene");
-        // 플레이어 이름에 시작화면에서 입력한 이름넣어주기
-        PlayerPrefs.SetString("PlayerName", playerName);
+        if(playerName.Length > 1 && playerName.Length < 11)
+        {
+            SceneManager.LoadScene("MainScene");
+            // 플레이어 이름에 시작화면에서 입력한 이름넣어주기
+            PlayerPrefs.SetString("PlayerName", playerName);
+        }
+
     }
 }
